f948fcdbed2ef9f54112965e4fd3379503ba6b3a
m.muravev <m.muravev@collegeschedule.ru>
2024-05-03 17:37:52 +0700

    LOADLOADLOAD

---
+++ XyrenthWeb/XyrenthWeb/Program.cs
@@ -0,0 +1,30 @@
+namespace XyrenthWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHttpsRedirection();
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
